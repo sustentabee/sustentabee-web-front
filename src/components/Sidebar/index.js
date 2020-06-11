49083c298198235d88aa6699ee66134c1205d580
Gustavo Aldar <gustavoaldar@outlook.com>
2020-06-11 11:09:26 -0300

    sidebar - updated

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import { Accordion, Card } from 'react-bootstrap';
+// import Button from 'react-bootstrap/Button';
 import Logo from "../../assets/img/bee.svg";
 import MaterialIcon from "material-icons-react";
 
@@ -17,15 +18,18 @@ export default class Sidebar extends Component {
                             <img src={Logo} className="img-fluid px-3" alt="Sustentabee" />
                             <h3 className="font-weight-bold" style={{ color: "#211F25" }}>Sustentabee</h3>
                         </div>
-                        <div className="d-flex align-items-center justify-content-center px-2">
+                        <div className="d-flex align-items-center justify-content-center px-2 pt-5">
                             <Accordion className="w-100">
                                 <SidebarNavLink link="/home" icon="home" name="Home" />
-                                <SidebarNavLink link="/equipamentos" icon="build" name="Equipamentos" />
-                                <SidebarNavLink link="/dashboard" icon="dashboard" name="Dashboards" />
-                                {/* <SidebarNavDropdown icon="dashboard" name="People View" >
-                                    <SidebarNavDropdownLink link="#" name="Clientes" />
-                                    <SidebarNavDropdownLink link="#" name="Usuários" />
-                                </SidebarNavDropdown> */}
+                                <SidebarNavDropdown icon="bar_chart" name="Dashboards" eventKey={0}>
+                                    <SidebarNavDropdownLink link={"/dashboard"} name="Dashboard" />
+                                    <SidebarNavDropdownLink link={"/dashboard"} name="Dashboard" />
+                                    <SidebarNavDropdownLink link={"/dashboard"} name="Dashboard" />
+                                    <SidebarNavDropdownLink link={"/dashboard"} name="Dashboard" />
+                                </SidebarNavDropdown>
+                                <SidebarNavLink link="#" icon="history" name="Medições" />
+                                <SidebarNavLink link="/equipamentos" icon="sensor_window" name="Equipamentos" />
+                                <SidebarNavLink link="#" icon="handyman" name="Manutenções" />
                             </Accordion>
                         </div>
                     </div>
@@ -41,37 +45,38 @@ function SidebarNavLink(props) {
         <Card className="mb-2 rounded">
             <Card.Header>
                 <Accordion.Toggle as={Link} to={props.link}>
-                    <MaterialIcon icon={props.icon} />
-                    <span className="ml-3 pt-1">{props.name}</span>
+                    <span style={{ fontSize: "24px", lineHeight: 0 }}><MaterialIcon icon={props.icon} /></span>
+                    <span className="ml-3 mt-1">{props.name}</span>
                 </Accordion.Toggle>
             </Card.Header>
         </Card>
     );
 }
 
-// function SidebarNavDropdown(props) {
-//     return (
-//         <Card className="mb-2 rounded">
-//             <Card.Header>
-//                 <Accordion.Toggle as={Link} to={"#"} eventKey="0">
-//                     <MaterialIcon icon={props.icon} />
-//                     <span className="ml-2">{props.name}</span>
-//                 </Accordion.Toggle>
-//             </Card.Header>
-//             <Accordion.Collapse eventKey="0">
-//                 <Card.Body>
-//                     {props.children}
-//                 </Card.Body>
-//             </Accordion.Collapse>
-//         </Card>
-//     );
-// }
+function SidebarNavDropdown(props) {
+    return (
+        <Card className="mb-2 rounded">
+            <Card.Header>
+                <Accordion.Toggle as={Link} to={"#"} eventKey={props.eventKey} className="d-flex align-items-center justify-content-between">
+                    <span style={{ fontSize: "24px", lineHeight: 0 }}><MaterialIcon icon={props.icon} /></span>
+                    <span className="ml-3 mt-1">{props.name}</span>
+                    <span className="ml-auto" style={{ fontSize: "24px", lineHeight: 0 }}><MaterialIcon icon={"keyboard_arrow_down"} /></span>
+                </Accordion.Toggle>
+            </Card.Header>
+            <Accordion.Collapse eventKey={props.eventKey}>
+                <Card.Body>
+                    {props.children}
+                </Card.Body>
+            </Accordion.Collapse>
+        </Card>
+    );
+}
 
-// function SidebarNavDropdownLink(props) {
-//     return (
-//         <Link to={props.link} className="d-flex align-items-center">
-//             <MaterialIcon icon="chevron_right" />
-//             <span className="ml-2">{props.name}</span>
-//         </Link>
-//     );
-// }
+function SidebarNavDropdownLink(props) {
+    return (
+        <Link to={props.link} className="d-flex align-items-center">
+            <span style={{ color: "transparent", fontSize: "24px", lineHeight: 0 }}><MaterialIcon icon="chevron_right" /></span>
+            <span className="ml-3 mt-1">{props.name}</span>
+        </Link>
+    );
+}
